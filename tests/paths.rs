1b8479ebf7c8c0a436357a361b798b126de0857a
Aleksandr Fedorov <canxes@mail.ru>
2021-09-24 18:20:06 +0300

    regress testing

--- tests/paths.rs
+++ tests/paths.rs
@@ -1,15 +1,47 @@
-use std::path::Path;
 use std::path::PathBuf;
 
-const RESOURCES: PathBuf = PathBuf::from(file!()).parent().unwrap().join("resources");
+use static_init::dynamic;
+
+#[dynamic]
+pub static RESOURCES: PathBuf = PathBuf::from(file!()).parent().unwrap().join("resources");
 
 #[allow(non_snake_case)]
 pub mod GRCh38 {
     use super::*;
 
-    const FOLDER: PathBuf = RESOURCES.join("GRCh38");
+    #[dynamic]
+    pub static FOLDER: PathBuf = RESOURCES.join("GRCh38");
 
-    pub fn gtf() -> &'static Path {
-        &FOLDER.join("Homo_sapiens.GRCh38.104.gtf.gz")
-    }
+    #[dynamic]
+    pub static GFF3: String = FOLDER.join("Homo_sapiens.GRCh38.104.gff3.gz").to_str().unwrap().to_string();
+
+    #[dynamic]
+    pub static FASTA: String =
+        FOLDER.join("Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz").to_str().unwrap().to_string();
+
+    #[dynamic]
+    pub static ALU: String = FOLDER.join("Alu.bed.gz").to_str().unwrap().to_string();
+}
+
+pub mod bam {
+    use super::*;
+
+    #[dynamic]
+    pub static FOLDER: PathBuf = RESOURCES.join("bam");
+
+    #[dynamic]
+    pub static EXAMPLE: String = FOLDER.join("SRX6966474.bam").to_str().unwrap().to_string();
+}
+
+pub mod expected {
+    use super::*;
+
+    #[dynamic]
+    pub static FOLDER: PathBuf = RESOURCES.join("expected");
+
+    #[dynamic]
+    pub static LOCI: PathBuf = FOLDER.join("loci");
+
+    #[dynamic]
+    pub static ROIS: PathBuf = FOLDER.join("rois");
 }
